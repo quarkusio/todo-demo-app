019fd19c2e3e58307274c1c56c7488277e5b3162
Bartosz Majsak <bartosz.majsak@gmail.com>
2020-09-27 11:27:38 +0200

    feat: adds parameterized test for REST delete

--- src/test/java/io/quarkus/sample/TodoResourceTest.java
+++ src/test/java/io/quarkus/sample/TodoResourceTest.java
@@ -1,11 +1,16 @@
 package io.quarkus.sample;
 
+import java.util.stream.Stream;
+
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
@@ -60,21 +65,33 @@ public class TodoResourceTest {
             .body(is(UPDATED));
     }
 
-    @Test
-    @Order(5)
-    public void testDelete() {
+    @ParameterizedTest @Order(5)
+    @MethodSource("todoItemsToDelete")
+    public void testDelete(int id, int expectedStatus) {
         given()
-                .pathParam("id", 5)
+                .pathParam("id", id)
         .when()
                 .delete("/api/{id}")
         .then()
-                .statusCode(204);
+                .statusCode(expectedStatus);
+    }
+
+    private static Stream<Arguments> todoItemsToDelete() {
+        return Stream.of(
+                Arguments.of(5, 204),
+                Arguments.of(15, 404)
+        );
     }
 
     private static final String ALL = "[{\"id\":1,\"completed\":true,\"order\":0,\"title\":\"Introduction to Quarkus\"},{\"id\":2,\"completed\":false,\"order\":1,\"title\":\"Hibernate with Panache\"},{\"id\":3,\"completed\":false,\"order\":2,\"title\":\"Visit Quarkus web site\",\"url\":\"https://quarkus.io\"},{\"id\":4,\"completed\":false,\"order\":3,\"title\":\"Star Quarkus project\",\"url\":\"https://github.com/quarkusio/quarkus/\"}]";
+
     private static final String ONE = "{\"id\":1,\"completed\":true,\"order\":0,\"title\":\"Introduction to Quarkus\"}";
+
     private static final String CREATE_NEW = "{\"completed\":false,\"order\":0,\"title\":\"Use the REST Endpoint\"}";
+
     private static final String NEW_CREATED = "{\"id\":5,\"completed\":false,\"order\":0,\"title\":\"Use the REST Endpoint\"}";
+
     private static final String UPDATE = "{\"id\":5,\"completed\":false,\"order\":0,\"title\":\"Use the GraphQL Endpoint\"}";
+
     private static final String UPDATED = "{\"id\":5,\"completed\":false,\"order\":0,\"title\":\"Use the GraphQL Endpoint\"}";
 }
