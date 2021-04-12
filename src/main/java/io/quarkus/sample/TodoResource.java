4e029446520884a069774ab46500c5986cf0f928
Michael Bornholdt Nielsen <michaelbornholdtnielsen@gmail.com>
2021-04-12 15:07:14 +0200

    Add use of Micrometer
    Upgrade to quarkus 1.13.1.Final
    Add Use of MeterRegistry TodoResource#create
    Add Metrics section to README.md
    Add start Postgres with podman to README.md

--- src/main/java/io/quarkus/sample/TodoResource.java
+++ src/main/java/io/quarkus/sample/TodoResource.java
@@ -1,85 +1,110 @@
 package io.quarkus.sample;
 
-import io.quarkus.panache.common.Sort;
+import java.util.List;
 
+import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
-import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.OPTIONS;
+import javax.ws.rs.PATCH;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
-import java.util.List;
+
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
+import io.micrometer.core.instrument.Timer.Sample;
+import io.quarkus.panache.common.Sort;
 
 @Path("/api")
 @Tag(name = "Todo Resource", description = "All Todo Operations")
 public class TodoResource {
 
-    @OPTIONS
-    @Operation(hidden = true)
-    public Response opt() {
-        return Response.ok().build();
-    }
+	@Inject
+	MeterRegistry registry;
 
-    @GET
-    @Operation(description = "Get all the todos")
-    public List<Todo> getAll() {
-        return Todo.listAll(Sort.by("order"));
-    }
+	@OPTIONS
+	@Operation(hidden = true)
+	public Response opt() {
+		return Response.ok().build();
+	}
 
-    @GET
-    @Path("/{id}")
-    @Operation(description = "Get a specific todo by id")
-    public Todo getOne(@PathParam("id") Long id) {
-        Todo entity = Todo.findById(id);
-        if (entity == null) {
-            throw new WebApplicationException("Todo with id of " + id + " does not exist.", Status.NOT_FOUND);
-        }
-        return entity;
-    }
+	@GET
+	@Operation(description = "Get all the todos")
+	public List<Todo> getAll() {
+		return Todo.listAll(Sort.by("order"));
+	}
 
-    @POST
-    @Transactional
-    @Operation(description = "Create a new todo")
-    public Response create(@Valid Todo item) {
-        item.persist();
-        return Response.status(Status.CREATED).entity(item).build();
-    }
+	@GET
+	@Path("/{id}")
+	@Operation(description = "Get a specific todo by id")
+	public Todo getOne(@PathParam("id") Long id) {
+		Todo entity = Todo.findById(id);
+		if (entity == null) {
+			throw new WebApplicationException("Todo with id of " + id + " does not exist.", Status.NOT_FOUND);
+		}
+		return entity;
+	}
 
-    @PATCH
-    @Path("/{id}")
-    @Transactional
-    @Operation(description = "Update an exiting todo")
-    public Response update(@Valid Todo todo, @PathParam("id") Long id) {
-        Todo entity = Todo.findById(id);
-        entity.id = id;
-        entity.completed = todo.completed;
-        entity.order = todo.order;
-        entity.title = todo.title;
-        entity.url = todo.url;
-        return Response.ok(entity).build();
-    }
+	@POST
+	@Transactional
+	@Operation(description = "Create a new todo")
+	public Response create(@Valid Todo item) {
+		
+		// Measure of how long it takes to perform a create.
+		Timer timer = Timer.builder("createTimer").register(registry);		
+		Sample sample = Timer.start(registry);
+		item.persist();
+		Response response = Response.status(Status.CREATED).entity(item).build();
+		sample.stop(timer);
+		
+		// How many create have been performed.		
+		this.registry.counter("createPerformed").increment();
+		
+		return response;
+	}
 
-    @DELETE
-    @Transactional
-    @Operation(description = "Remove all completed todos")
-    public Response deleteCompleted() {
-        Todo.deleteCompleted();
-        return Response.noContent().build();
-    }
+	@PATCH
+	@Path("/{id}")
+	@Transactional
+	@Operation(description = "Update an exiting todo")
+	public Response update(@Valid Todo todo, @PathParam("id") Long id) {
+		Todo entity = Todo.findById(id);
+		entity.id = id;
+		entity.completed = todo.completed;
+		entity.order = todo.order;
+		entity.title = todo.title;
+		entity.url = todo.url;
+		return Response.ok(entity).build();
+	}
 
-    @DELETE
-    @Transactional
-    @Path("/{id}")
-    @Operation(description = "Delete a specific todo")
-    public Response deleteOne(@PathParam("id") Long id) {
-        Todo entity = Todo.findById(id);
-        if (entity == null) {
-            throw new WebApplicationException("Todo with id of " + id + " does not exist.", Status.NOT_FOUND);
-        }
-        entity.delete();
-        return Response.noContent().build();
-    }
+	@DELETE
+	@Transactional
+	@Operation(description = "Remove all completed todos")
+	public Response deleteCompleted() {
+		Todo.deleteCompleted();
+		return Response.noContent().build();
+	}
+
+	@DELETE
+	@Transactional
+	@Path("/{id}")
+	@Operation(description = "Delete a specific todo")
+	public Response deleteOne(@PathParam("id") Long id) {
+		Todo entity = Todo.findById(id);
+		if (entity == null) {
+			throw new WebApplicationException("Todo with id of " + id + " does not exist.", Status.NOT_FOUND);
+		}
+		entity.delete();
+		return Response.noContent().build();
+	}
 
 }
